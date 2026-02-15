bc90df91aa5c4b6ebcf94024eb8100abf744d429
danyowoj <latsuk1803@gmail.com>
2026-02-15 11:16:06 +0700

    Конвертер. Добавил класс History

---
+++ Converter/History.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+#include <vector>
+
+class History
+{
+public:
+    struct Record
+    {
+        int p1;
+        int p2;
+        std::string num1;
+        std::string num2;
+
+        Record(int p1, int p2, const std::string &n1, const std::string &n2);
+        std::string toString() const;
+    };
+
+    History();
+
+    // Добавить запись в историю
+    void addRecord(int p1, int p2, const std::string &n1, const std::string &n2);
+
+    // Получить запись по индексу
+    Record getRecord(int index) const;
+
+    // Оператор доступа по индексу
+    Record operator[](int index) const;
+
+    // Очистить историю
+    void clear();
+
+    // Количество записей в итсории
+    int count() const;
+
+private:
+    std::vector<Record> records;
+};
