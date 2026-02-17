668fdf845b23048f480e5fcda8773212824f68e0
danyowoj <latsuk1803@gmail.com>
2026-02-17 12:14:53 +0700

    Конвертер. Обновил справку

--- ConverterQt/AboutDialog.cpp
+++ ConverterQt/AboutDialog.cpp
@@ -7,18 +7,48 @@ AboutDialog::AboutDialog(QWidget *parent)
     : QDialog(parent)
 {
     setWindowTitle("О программе");
-    resize(400, 300);
+    resize(400, 600); // увеличен размер для размещения справки
 
     QVBoxLayout *layout = new QVBoxLayout(this);
-    QLabel *label = new QLabel(
-        "<h4>Конвертор p1-p2</h4>"
-        "<p>Преобразование действительных чисел из системы с основанием p1 в систему с основанием p2.</p>"
-        "<p>Разработчики: danyowoj & opepl.</p>"
+
+    // Заголовок
+    QLabel *titleLabel = new QLabel("<h2>Конвертор p1-p2</h2>");
+    titleLabel->setAlignment(Qt::AlignCenter);
+    layout->addWidget(titleLabel);
+
+    // Описание программы
+    QLabel *descLabel = new QLabel(
+        "<p><b>Назначение:</b> Преобразование действительных чисел "
+        "из системы счисления с основанием p1 в систему с основанием p2.</p>"
         );
+    descLabel->setWordWrap(true);
+    layout->addWidget(descLabel);
 
-    label->setWordWrap(true);
-    layout->addWidget(label);
+    // Справка по использованию
+    QLabel *helpLabel = new QLabel(
+        "<p><b>Использование:</b></p>"
+        "<ul>"
+        "<li>Выберите основание p1 (исходное число) и p2 (результат) с помощью слайдеров или счётчиков.</li>"
+        "<li>Введите число, используя кнопки с цифрами или клавиатуру (0-9, A-F).</li>"
+        "<li>Нажмите <b>Exec</b> или клавишу <b>Enter</b> для преобразования.</li>"
+        "<li>Кнопка <b>.</b> добавляет десятичный разделитель.</li>"
+        "<li>Кнопка <b>BS</b> удаляет последний символ (Backspace).</li>"
+        "<li>Кнопка <b>CE</b> очищает поле ввода.</li>"
+        "<li>Меню <b>История</b> показывает все выполненные преобразования.</li>"
+        "</ul>"
+        "<p><i>Примечание:</i> недопустимые для выбранного основания цифры автоматически блокируются.</p>"
+        );
+    helpLabel->setWordWrap(true);
+    layout->addWidget(helpLabel);
 
+    // Разработчики
+    QLabel *devLabel = new QLabel(
+        "<p><b>Разработчики:</b> danyowoj & opepl</p>"
+        );
+    devLabel->setAlignment(Qt::AlignCenter);
+    layout->addWidget(devLabel);
+
+    // Кнопка OK
     QPushButton *okBtn = new QPushButton("OK");
     connect(okBtn, &QPushButton::clicked, this, &QDialog::accept);
     layout->addWidget(okBtn, 0, Qt::AlignCenter);
